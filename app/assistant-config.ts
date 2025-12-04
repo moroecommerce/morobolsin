1e94bdebf4229e15cd02574d3c3dd46e8c368d2b
moroecommerce <morobolsin.uz@gmail.com>
2025-12-04 17:28:24 +0500

    Update assistant-config.ts

--- app/assistant-config.ts
+++ app/assistant-config.ts
@@ -1,4 +1,4 @@
-export let assistantId = ""; // set your assistant ID here
+export let assistantId = "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"; // set your assistant ID here
 
 if (assistantId === "") {
   assistantId = process.env.OPENAI_ASSISTANT_ID;
